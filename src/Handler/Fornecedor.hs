1bbd383b6121183150d5aabce32d65d580faa01c
Anderson Morais <dessomorais@hotmail.com>
2017-11-30 02:06:52 +0000

    Criando formFornecedorR, getCadastrarFornecedorR, postCadastrarFornecedorR e getListarFornecedorR

--- src/Handler/Fornecedor.hs
+++ src/Handler/Fornecedor.hs
@@ -18,17 +18,100 @@ import Database.Persist.Postgresql
 -- /fornecedor/editar/#FornecedorId    EditarFornecedorR     PUT
 -- /fornecedor/excluir/#FornecedorId   ExcluirFornecedorR    POST
 
+-- Fornecedor json
+--     cnpj              Text
+--     nomeEmpresa       Text
+--     nome              Text  
+--     email             Text  
+--     telefonecomercial Text  
+--     cargo             Text  
+--     Uniquecnpj        cnpj
+--     deriving Show
+
+-- formulário de Cadastro de Fornecedores
+formFornecedor :: Form Fornecedor
+formFornecedor = renderDivs $ Fornecedor
+    <$> areq textField     "CNPJ: "                 Nothing
+    <*> areq textField     "Nome da Empresa: "      Nothing
+    <*> areq textField     "Nome do Fornecedor: "   Nothing
+    <*> areq emailField    "Email: "                Nothing
+    <*> areq textField     "Telefone Comercial: "   Nothing
+    <*> areq textField     "Cargo: "                Nothing
+    
+-- <*> areq (selectField $ optionsPersistKey [] [] tipousuarioDescricao) "Tipo Usuario: " Nothing
+-- <*> (fmap toSqlKey $ areq intField "Tipo Usuario" Nothing)
+
 getFornecedorR :: Handler Html
-getFornecedorR = error "undefined"
+getFornecedorR = do
+    defaultLayout $ do
+        toWidget [lucius|
+            li {
+                display: inline-block;
+                list-style:  none;
+            }
+            
+        |]
+        [whamlet|
+            <h1> Fornecedores
+            <ul>
+                <li> <a href=@{CadastrarFornecedorR}>  Cadastrar Fornecedor
+                <li> <a href=@{ListarFornecedorR}>  Listar Fornecedor
+                <li> <a href=@{HomeR}>  Home
+        |]
 
+-- essa função pega um formulário (FormFornecedor) e gera um Handler com uma tupla de Widget (campos do form)e um ency
 getCadastrarFornecedorR :: Handler Html
-getCadastrarFornecedorR = error "undefined"
-
+getCadastrarFornecedorR = do
+        (widget,enctype) <- generateFormPost formFornecedor
+    defaultLayout $ do
+        [whamlet|
+            <form action=@{CadastrarFornecedorR} method=post>
+                ^{widget}
+                <input type="submit" value="Cadastrar">
+        |]
+    
 postCadastrarFornecedorR :: Handler Html 
-postCadastrarFornecedorR = error "undefined"
+postCadastrarFornecedorR = do
+    ((res,_),_) <- runFormPost formFornecedor
+    case res of 
+        FormSuccess func -> do
+            _ <- runDB $ insert func
+            redirect FornecedorR
+        _ -> do
+            setMessage $ [shamlet| Falha no Cadastro |]
+            redirect CadastrarFornecedorR    
 
 getListarFornecedorR :: Handler Html
-getListarFornecedorR = error "undefined"
+getListarFornecedorR = do
+    usuarios <- runDB $ selectList [] [Asc FornecedorNome]
+    defaultLayout $ do
+        [whamlet| 
+            <table>
+                <thead>
+                    <tr>
+                        <th>
+                            NOME
+                        <th>
+                            CPF
+                        <th>
+                            E-MAIL
+                <tbody>
+                    $forall (Entity usuarioid usuario) <- usuarios
+                        <tr>
+                            <td>
+                                <a href=@{PerfilUsuarioR usuarioid}> 
+                                    #{usuarioNome usuario}
+                            
+                            <td>
+                                #{usuarioCpf usuario}
+                            
+                            <td>
+                                #{usuarioEmail usuario}
+                            
+                            <td>
+                                <form action=@{ExcluirUsuarioR usuarioid} method=post>
+                                    <input type="submit" value="Excluir">
+        |]
 
 getBuscarFornecedorR :: FornecedorId -> Handler Html
 getBuscarFornecedorR = error "undefined"
