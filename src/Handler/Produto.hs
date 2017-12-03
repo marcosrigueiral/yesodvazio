e47c3b6938e71ac6f7b9e2de13c81022900c4e39
Gabriel Fonseca <gabrielfonseca.coutinho@outlook.com>
2017-12-03 16:30:00 +0000

    Alteração na tela de cadastro de produtos

--- src/Handler/Produto.hs
+++ src/Handler/Produto.hs
@@ -69,10 +69,91 @@ getCadastrarProdutoR :: Handler Html
 getCadastrarProdutoR = do 
     (widget,enctype) <- generateFormPost formProduto
     defaultLayout $ do
+        addStylesheet $ (StaticR css_bootstrap_css)
+        --addStylesheet $ (StaticR css_temas_css)
+        addScript $ (StaticR js_jquery_min_js)
+        addScript $ (StaticR js_bootstrap_min_js)
+        --corpo html
+        -- $(whamletFile "templates/home.hamlet")
+        toWidget [lucius|
+            .logo-menu 
+            {
+                float: left;
+                width: 140px;
+                margin-top: -10px;
+            }
+            .btn-sair 
+            {
+                float: right;
+                margin-top: 8px;
+            }
+        |]        
         [whamlet|
-            <form action=@{CadastrarProdutoR} method=post>
-                ^{widget}
-                <input type="submit" value="Cadastrar">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <nav class="navbar navbar-inverse">
+                <div class="container">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                            <span class="sr-only">
+                                Toggle navigation
+                            <span class="icon-bar">
+                            <span class="icon-bar">
+                            <span class="icon-bar">
+                        <a class="navbar-brand" href="#">
+                            <img src="../../static/img/logo-2.png" class="logo-menu" />
+                    <div id="navbar" class="collapse navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                    Produtos
+                                    <span class="caret">
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href=@{CadastrarProdutoR}>
+                                            Cadastro de Produtos
+                                    <li>
+                                        <a href=@{ListarProdutoR}>
+                                            Listagem de Produtos
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                    Categorias
+                                    <span class="caret">
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href=@{CadastrarCategoriaR}>
+                                            Cadastro de Categorias
+                                    <li>
+                                        <a href=@{ListarCategoriaR}>
+                                            Listagem de Categorias 
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                    Fornecedores
+                                    <span class="caret">
+                                <ul class="dropdown-menu">
+                                    <li>
+                                        <a href=@{CadastrarFornecedorR}>
+                                            Cadastro de Fornecedores
+                                    <li>
+                                        <a href=@{ListarCategoriaR}>
+                                            Listagem de Fornecedores                                            
+                            <li> 
+                                <a href=@{UsuarioR}> Cadastro de Usuarios
+                                    <li>
+                                        <form action=@{LogoutR} method=post>
+                                            <button type="submit" value="" class="btn btn-danger btn-sair">
+                                                Sair
+                                                <span class="glyphicon glyphicon-remove" aria-hidden="true">                        
+                                            
+                                                
+            <div class="container">   
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <span class="glyphicon glyphicon-th-list" aria-hidden="true">
+                        Listagem de Produtos        
+                    <div class="panel-body">
+                        <form action=@{CadastrarProdutoR} method=post>
+                            ^{widget}
+                            <input type="submit" value="Cadastrar">
         |]
 
 postCadastrarProdutoR :: Handler Html 
