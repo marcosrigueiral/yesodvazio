e007d463d8e512a98075e10036de5ad92f25f556
Anderson Morais <dessomorais@hotmail.com>
2017-12-05 14:12:50 +0000

    Rotas getCadastrarPedido e postCadastrarPedido

--- src/Handler/Pedido.hs
+++ src/Handler/Pedido.hs
@@ -46,32 +46,7 @@ formPedido = renderDivs $ Pedido
                                       fsTooltip= Nothing,
                                       fsName= Nothing,
                                       fsAttrs=[("class","form-control")]} Nothing
-    -- <*> areq textField FieldSettings{fsId=Just "txtCodigo",
-    --                                   fsLabel="Código: ",
-    --                                   fsTooltip= Nothing,
-    --                                   fsName= Nothing,
-    --                                   fsAttrs=[("class","form-control")]} Nothing
-    -- <*> areq doubleField FieldSettings{fsId=Just "txtCusto",
-    --                                   fsLabel="Custo: ",
-    --                                   fsTooltip= Nothing,
-    --                                   fsName= Nothing,
-    --                                   fsAttrs=[("class","form-control")]} Nothing
-    -- <*> areq doubleField FieldSettings{fsId=Just "txtPreco",
-    --                                   fsLabel="Preço: ",
-    --                                   fsTooltip= Nothing,
-    --                                   fsName= Nothing,
-    --                                   fsAttrs=[("class","form-control")]} Nothing
-    -- <*> areq intField FieldSettings{fsId=Just "txtEstoque",
-    --                                   fsLabel="Estoque: ",
-    --                                   fsTooltip= Nothing,
-    --                                   fsName= Nothing,
-    --                                   fsAttrs=[("class","form-control")]} Nothing
-    -- <*> areq (selectField $ optionsPersistKey [] [] fornecedorNome) FieldSettings{fsId=Just "txtFornecedor",
-    --                                   fsLabel="Fornecedor: ",
-    --                                   fsTooltip= Nothing,
-    --                                   fsName= Nothing,
-    --                                   fsAttrs=[("class","form-control")]} Nothing
-
+    
 -- getFuncionarioR :: Handler TypedContent
 -- getFuncionarioR = do
 --     funcionario <- (runDB $ selectList [FuncionarioExcluido ==. False][Asc FuncionarioNome]) :: Handler [Entity Funcionario]
@@ -95,3 +70,166 @@ getPedidoR = do
                 <li> <a href=@{HomeR}>  Home
         |]
 
+-- preenchimento de formulario para cadastro de Funcionarios utilizando o form criado acima
+getCadastrarPedidoR :: Handler Html
+getCadastrarPedidosR = do 
+    (widget,enctype) <- generateFormPost formPedido
+    defaultLayout $ do
+        addStylesheet $ (StaticR css_bootstrap_css)
+        addScript $ (StaticR js_jquery_min_js)
+        addScript $ (StaticR js_bootstrap_min_js)
+        --corpo html
+        -- $(whamletFile "templates/home.hamlet")
+        toWidget [lucius|
+            .logo-menu 
+            {
+                float: left;
+                width: 140px;
+                margin-top: -10px;
+            }
+            .btn-sair 
+            {
+                float: right;
+                margin-top: 8px;
+            }
+            .required
+            {
+                width: 200px;
+                float: left;
+                padding: 10px;
+            }
+            .btn-area
+            {
+                width: 100%;
+                float: left;
+            }
+            .btn-enviar
+            {
+                float: right;
+            }
+        |]        
+        [whamlet|
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+                <nav class="navbar navbar-inverse">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span class="sr-only">
+                                    Toggle navigation
+                                <span class="icon-bar">
+                                <span class="icon-bar">
+                                <span class="icon-bar">
+                            <a class="navbar-brand" href=@{HomeR}>
+                                <img src="../../static/img/logo-2.png" class="logo-menu" />
+                        <div id="navbar" class="collapse navbar-collapse">
+                            <ul class="nav navbar-nav">
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        Produtos
+                                        <span class="caret">
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href=@{CadastrarProdutoR}>
+                                                Cadastro de Produtos
+                                        <li>
+                                            <a href=@{ListarProdutoR}>
+                                                Listagem de Produtos
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        Categorias
+                                        <span class="caret">
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href=@{CadastrarCategoriaR}>
+                                                Cadastro de Categorias
+                                        <li>
+                                            <a href=@{ListarCategoriaR}>
+                                                Listagem de Categorias 
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        Pedidos
+                                        <span class="caret">
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href=@{CadastrarPedidoR}>
+                                                Cadastro de Pedidos
+                                        <li>
+                                            <a href=@{ListarPedidoR}>
+                                                Listagem de Pedidos
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        Fornecedores
+                                        <span class="caret">
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href=@{CadastrarFornecedorR}>
+                                                Cadastro de Fornecedores
+                                        <li>
+                                            <a href=@{ListarFornecedorR}>
+                                                Listagem de Fornecedores  
+                                                
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        Funcionários
+                                        <span class="caret">
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href=@{CadastrarFuncionarioR}>
+                                                Cadastro de Funcionários
+                                        <li>
+                                            <a href=@{ListarFuncionarioR}>
+                                                Listagem de Funcionários  
+                                                
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                        Clientes
+                                        <span class="caret">
+                                    <ul class="dropdown-menu">
+                                        <li>
+                                            <a href=@{CadastrarClienteR}>
+                                                Cadastro de Clientes
+                                        <li>
+                                            <a href=@{ListarClienteR}>
+                                                Listagem de Clientes  
+                                
+                                <li>
+                                    <form action=@{LogoutR} method=post>
+                                        <button type="submit" value="" class="btn btn-danger btn-sair">
+                                            Sair
+                                            <span class="glyphicon glyphicon-remove" aria-hidden="true">                        
+            <div class="container">   
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <span class="glyphicon glyphicon-download-alt" aria-hidden="true">
+                        Cadastro de Funcionários        
+                    <div class="panel-body">             
+                        <form action=@{CadastrarFuncionarioR} method=post>
+                            ^{widget}
+                            <div class="btn-area">                            
+                                <button type="submit" value="" class="btn btn-success btn-enviar">
+                                    Cadastrar
+                                    <span class="glyphicon glyphicon-plus" aria-hidden="true">                            
+        |]
+
+getBuscarPedidoR :: PedidoId -> Handler Html
+getBuscarPedidoR = erro "undefined"
+
+-- getFuncionarioIdR :: FuncionarioId -> Handler TypedContent
+-- getFuncionarioIdR funcionarioId = do
+--     funcionario <- runDB $ get404 funcionarioId
+--     cidade <- runDB $ get404 $ funcionarioCidadeId funcionario
+--     estado <- runDB $ get404 $ funcionarioEstadoId $  funcionario
+--     sendStatusJSON ok200 $ object ["Funcionario" .= funcionario, "Cidade" .= cidade, "Estado" .= estado]
+
+
+-- inclusao do formulario preenchido no banco
+postCadastrarPedidoR :: Handler Html
+postCadastrarPedidoR = do 
+    ((res,_),_) <- runFormPost formPedido
+    case res of 
+        FormSuccess ped -> do
+            _ <- runDB $ insert ped
+            redirect PedidoR
+        _ -> do
+            setMessage $ [shamlet| Falha no Cadastro |]
+            redirect CadastrarPedidoR
